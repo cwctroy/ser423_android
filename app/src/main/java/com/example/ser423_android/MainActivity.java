8e0dd67a1d8cf0afdc3958081de98fb6e2e8ff3a
Cameron Troy <camtroy93@gmail.com>
2020-10-17 17:18:34 -0400

    initial commit of project

--- app/src/main/java/com/example/ser423_android/MainActivity.java
+++ app/src/main/java/com/example/ser423_android/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.ser423_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView myTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myTV = (TextView) findViewById(R.id.my_text_view);
+    }
+
+    public void buttonClicked(View v) {
+       android.util.Log.d(this.getClass().getSimpleName(), "button clicked");
+       myTV.setText(myTV.getText() + "ouch, ");
     }
 }
